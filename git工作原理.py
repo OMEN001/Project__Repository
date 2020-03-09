1a27ea8e2c52bfb59128da9f3e0a100dc6b3501b
OMEN001 <1668317403@qq.com>
2020-03-09 17:29:30 +0800

    学习了git的工作原理

---
+++ git工作原理.py
@@ -0,0 +1,19 @@
+#_*_conding:utf-8_*_
+#@Time    :2020/3/818:20
+#@Author  :xiaodong.wu
+#@Email   :2586089125@qq.com
+
+"""
+git的基本概念
+remote  远程仓库
+repository 本地仓库
+workspace 工作区  存放项目目录文件的区域，开发工作内容就是在工作区域进行（如：pycharm）
+index 暂存区  介于工作区间和本地仓库（版本库）之间，要提交到版本库的问价必须先暂存到这个区域
+
+最常用的git命令
+git add 文件名   --将工作区的指定文件添加的暂存区
+git commit -m "注释"  --将暂存区的文件提交到本地仓库并产生一个新的版本
+git push --将本地仓库的最新内容推送到远程
+
+"""
+
